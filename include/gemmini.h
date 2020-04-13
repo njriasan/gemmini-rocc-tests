30cd0012fa15857598cea7cda4af6a1bd58b491e
njriasan <njriasanovsky@berkeley.edu>
2020-04-12 17:16:49 -0700

    fixed representation of negative numbers

--- include/gemmini.h
+++ include/gemmini.h
@@ -104,11 +104,11 @@ void printMatrix_4bit(elem_t m[DIM][DIM/2]) {
     for (size_t j = 0; j < DIM/2; ++j) {
       elem_t m_elem = m[i][j/2];
       elem_t upper = (m_elem >> 4) & 0x0F;
-      elem_t sign = upper & 0x08;
+      elem_t sign = (upper >> 3) & 0x01;
       elem_t or_val = (sign << 7) | (sign << 6) | (sign << 5) | (sign << 4);
       upper = upper | or_val;
       elem_t lower = m_elem & 0x0F;
-      sign = lower & 0x08;
+      sign = (lower >> 3) & 0x01;
       or_val = (sign << 7) | (sign << 6) | (sign << 5) | (sign << 4);
       lower = lower | or_val;
       if (upper < 0) {
